6d76fdf48bf76140841f4920ad7535895b4e3ed1
Karan <karan_chgh@yahoo.com>
2024-02-20 12:53:46 -0500

    changed pet-app path

--- pipeline/build/mvn.sh
+++ pipeline/build/mvn.sh
@@ -4,8 +4,9 @@ echo "*************************************"
 echo "********** Building jar *************"
 echo "*************************************"
 
-WORKSPACE=/var/lib/jenkins/workspace/build
+#WORKSPACE=/var/lib/jenkins/workspace/build
+WORKSPACE=/home/ec2-user/build
 
 docker run --rm -v $WORKSPACE/petclinic-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.8-openjdk-18-slim "$@"
 
-cp petclinic-app/target/*.jar pipeline/build/
+cp $WORKSPACE/petclinic-app/target/*.jar pipeline/build/

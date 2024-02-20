6f187b6e2d35a9f95118ebea442a854024826d73
Karan <karan_chgh@yahoo.com>
2024-02-20 16:15:52 -0500

    fixed cp

--- pipeline/build/mvn.sh
+++ pipeline/build/mvn.sh
@@ -9,4 +9,4 @@ WORKSPACE=/home/ec2-user/build
 
 docker run --rm -v $WORKSPACE/petclinic-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.8-openjdk-18-slim "$@"
 
-cp $WORKSPACE/petclinic-app/target/*.jar $WORKSPACE/pipeline/build/
+cp $WORKSPACE/petclinic-app/target/*.jar $WORKSPACE/

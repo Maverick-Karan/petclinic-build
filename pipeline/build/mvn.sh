b3428013570241c39ff37fb4ca8d23bc34867b19
Karan <karan_chgh@yahoo.com>
2024-02-20 16:46:00 -0500

    opy path

--- pipeline/build/mvn.sh
+++ pipeline/build/mvn.sh
@@ -9,6 +9,5 @@ WORKSPACE=/home/ec2-user/build
 
 docker run --rm -v $WORKSPACE/petclinic-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.8-openjdk-18-slim "$@"
 
-whoami
-
-cp $WORKSPACE/petclinic-app/target/*.jar $WORKSPACE/
+ls -l $WORKSPACE/petclinic-app/target/*.jar
+#cp $WORKSPACE/petclinic-app/target/*.jar $WORKSPACE/pipeline/build/
